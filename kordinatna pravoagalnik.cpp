4d3296615347c5e1a5d65b2ec101d3791e358db2
Foker <140693336+megaqk@users.noreply.github.com>
2023-11-14 19:24:50 +0200

    Create kordinatna pravoagalnik.cpp

---
+++ kordinatna pravoagalnik.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdlib.h>
+#include <ctime>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+int main() {
+
+    int x1;
+    cin >> x1;
+    int y1;
+    cin >> y1;
+    int x2;
+    cin >> x2;
+    int y2;
+    cin >> y2;
+    int x;
+
+    cin >> x;
+    int y;
+    cin >> y;
+
+    
+    if (x > x1 && x < x2 && y > y1 % y < y2) {
+        cout << "Inside\n";
+    }
+
+
+
+
+
+
+
+  return 0;
+}
