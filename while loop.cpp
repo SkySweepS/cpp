108f2ae8d7d363449d34b89a0b6450945cb51d0a
Foker <140693336+megaqk@users.noreply.github.com>
2023-11-18 12:37:08 +0200

    Update while loop.cpp

--- while loop.cpp
+++ while loop.cpp
@@ -94,7 +94,56 @@ int main() {
    cout << number++ << endl;
   // uvelichava sledvashtiq number
    cout << number << endl;
-  
+
+
+
+
+  int main() {
+    string name;
+    cin >> name;
+
+    int grade = 1;
+
+    double totalMarks = 0;
+
+
+    while(grade <= 12) {
+
+        double mark;
+        cin >> mark;
+
+        if(mark >= 4.00) {
+            totalMarks += mark;
+            grade ++;
+        }
+        
+
+        
+
+
+        
+
+
+    }
+    grade --;
+    double average = totalMarks / grade;
+
+
+    cout.setf(ios::fixed);
+    cout.precision(2);
+
+    cout << name << " graduated. Average grade: " << average << endl;
+
+ 
+
+    
+   
+   
+
+
+
+  return 0;
+}
   
   
   
