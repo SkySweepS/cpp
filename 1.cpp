c772fd814f2f964266979a40b2096cce8c5640c8
Foker <140693336+megaqk@users.noreply.github.com>
2023-11-24 18:56:04 +0200

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <stdlib.h>
+#include <ctime>
+#include <vector>
+#include <string>
+#include <climits>
+
+using namespace std;
+
+
+int main() {
+
+  string name;
+  cin >> name;
+  double money;
+  cin >> money;
+  int beerBottles;
+  cin >> beerBottles;
+  int chipsPacks;
+  cin >> chipsPacks;
+
+  double beerPrice = 1.20;
+  double chipsPrice = 
+
+
+
+  return 0;
+}
