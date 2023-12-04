794714502dc2b642dd96bd99298492402cee67ae
SVP <140693336+megaqk@users.noreply.github.com>
2023-12-04 14:01:37 +0200

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -11,48 +11,53 @@
 
 using namespace std;
 
-bool isPrime(int number) {
-    if (number <= 1) {
-        return false;
-    }
-    for (int i = 2; i * i <= number; i++){
-        if (number % i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
 
 int main() {
-
-  int n1;
-  cin >> n1;
-  int n2;
-  cin >> n2;
-  int n3;
-  cin >> n3;
-
-  
     
-    for(int i1 = 2; i1 <= n1; i1 += 2){
-      int prime;
-     
-      for(prime = 2; prime <= n2; prime ++){
-          if(isPrime(prime)){
-
-        for(int i3 = 2; i3 <= n3; i3 += 2){
-          
-          cout << i1 << prime << i3 << endl;
-          
+    int k;
+    cin >> k;
+    int l;
+    cin >> l;
+    int m;
+    cin >> m;
+    int n;
+    cin >> n;
+    
+    int n1;
+    int n2;
+    int n3;
+    int n4;
+    int left;
+    
+    
+    for(n1 = k; n1 <= 8; n1++){
+        if(n1 % 2 == 0){
+        for(int n2 = 9; n2 >= l; n2 -= 2){
+            cout << n1 << n2 << endl;
+            
+            }
         }
-        
-      }
+    }
+    
+    for(n3 = m; n3 <= 8; n3++){
+        if(n3 % 2 == 0){
+        for (int n4 = 9; n4 >= n; n4 -= 2){
+            cout << n3 << n4 << endl;
+        }
     }
     }
-
-
-
+    
 
 
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
