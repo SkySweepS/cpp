0a189b19af3bc468e89dbaeb398cf484f74e77ef
SVP <140693336+megaqk@users.noreply.github.com>
2023-12-04 18:22:48 +0200

    Create funkcii2.cpp

---
+++ funkcii2.cpp
@@ -0,0 +1,19 @@
+//FUNKCIQ ZA IZPISVANE NA IME
+
+void introduction(std::string first_name, std::string last_name){
+  std::cout << last_name << ", " << first_name << " " << last_name;
+}
+////////////////////////////////////////////////////
+//FUNKCIQ ZA NAMIRANE NA SREDNO CHISLO 
+////////////////////////////////////////////////////
+double average(double num1, double num2){
+  double result = (num1 + num2) / 2;
+  return result;
+}
+////////////////////////////////////////////////////
+//FUNKCIQ ZA POVDIGANE NA STEPEN V SLUCHAQ NA 10ta
+////////////////////////////////////////////////////
+int tenth_power(int num){
+  return pow(num, 10);
+}
+////////////////////////////////////////////////////
