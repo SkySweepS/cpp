3f438c21ba00779db7c9684b43cfdf87587f50e6
SVP <140693336+megaqk@users.noreply.github.com>
2023-12-04 15:46:42 +0200

    Create IZPITNA ZADACHA 6 SMENI VAV FOOTBOLA.cpp

--- 1.cpp
+++ 1.cpp
@@ -33,19 +33,25 @@ int main() {
     for(n1 = k; n1 <= 8; n1++){
         if(n1 % 2 == 0){
         for(int n2 = 9; n2 >= l; n2 -= 2){
-            cout << n1 << n2 << endl;
-            
+            //cout << n1 << n2 << endl;
+            for(n3 = m; n3 <= 8; n3++){
+        if(n3 % 2 == 0){
+        for (int n4 = 9; n4 >= n; n4 -= 2){
+            if(n1 == n3 && n2 == n4){
+                cout << "Cannot change the same player." << endl;
+            }
+            else {
+            cout << n1 << n2 << " - " << n3 << n4 << endl;
+        }
+        }
+    }
+    }
+    
             }
         }
     }
     
-    for(n3 = m; n3 <= 8; n3++){
-        if(n3 % 2 == 0){
-        for (int n4 = 9; n4 >= n; n4 -= 2){
-            cout << n3 << n4 << endl;
-        }
-    }
-    }
+
     
 
 
