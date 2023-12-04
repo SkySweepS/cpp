3f139b2a7f4bee7aaa7ce8f448ddb88347be82e0
SVP <140693336+megaqk@users.noreply.github.com>
2023-12-05 00:13:08 +0200

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -6,64 +6,111 @@
 #include <stdlib.h>
 #include <ctime>
 #include <climits>
+#include <sstream>
 
 
 
 using namespace std;
 
 
+
+
 int main() {
-    
-    int k;
-    cin >> k;
-    int l;
-    cin >> l;
-    int m;
-    cin >> m;
-    int n;
-    cin >> n;
-    
-    int n1;
-    int n2;
-    int n3;
-    int n4;
-    int left;
-    
-    
-    for(n1 = k; n1 <= 8; n1++){
-        if(n1 % 2 == 0){
-        for(int n2 = 9; n2 >= l; n2 -= 2){
-            //cout << n1 << n2 << endl;
-            for(n3 = m; n3 <= 8; n3++){
-        if(n3 % 2 == 0){
-        for (int n4 = 9; n4 >= n; n4 -= 2){
-            if(n1 == n3 && n2 == n4){
-                cout << "Cannot change the same player." << endl;
-            }
-            else {
-            cout << n1 << n2 << " - " << n3 << n4 << endl;
+
+  double budget;
+  cin >> budget;
+  cin.ignore();
+  string mall;
+  int bought = 0;
+
+while(mall != "mall.Exit"){
+  getline(cin, mall);
+   cin.ignore();
+  if(mall == "mall.Enter"){
+    while(mall != "mall.Exit"){
+     getline(cin, mall);
+
+      if (mall == "mall.Exit"){
+        break;
+      }
+
+   
+        
+
+       vector<char> cV;
+       
+       for(char c : mall){
+        cV.push_back(c);
+        
+      }
+
+       for(char c : cV){
+        
+
+        if(c >= 'A' && c <= 'Z'){
+          double percent = c * 0.5;
+          budget -= percent;
+          bought++;
+          
         }
+        else if(c >= 'a' && c <= 'z'){
+          double percent = c * 0.3;
+          budget -= percent;
+          bought++;
+          
         }
-    }
-    }
-    
-            }
+        else if(c == '%'){
+          budget = budget / 2;
+          bought++;
+          
         }
-    }
-    
+        else if(c == '*'){
+          budget += 10;
+          
+        }
+        else{
+          budget -= c;
+          bought++;
+          
+        }
+       }
+       
+        
+        
+
+      
+      
+      
+  
 
     
 
+      
+      
+      if(budget <= 0){
+        break;
+      }
+      
+
+    
+    }
+  }
+  if(mall == "mall.Exit" || budget < 0){
+    break;
+  }
+}
+  cout.setf (ios::fixed);
+  cout.precision(2);
+  cout << bought << " purchases. Money left: " << budget << " lv." << endl;
+
+ 
+  
 
   return 0;
 }
 
+    
 
+    
 
-
-
-
-
-
-
-
+  
