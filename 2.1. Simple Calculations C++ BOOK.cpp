76f7d2874be1ee8da85fb87a34cca3df7af40cd3
SVP <140693336+megaqk@users.noreply.github.com>
2023-12-09 08:52:59 +0200

    Update 2.1. Simple Calculations C++ BOOK.cpp

--- 2.1. Simple Calculations C++ BOOK.cpp
+++ 2.1. Simple Calculations C++ BOOK.cpp
@@ -46,3 +46,40 @@ int main() {
 
   return 0;
 } 
+
+//ZADACHA 6
+
+int main() {
+
+  cout << "Enter circle radius. r = ";
+  double r;
+  cin >> r;
+  double pi = 3.14159265359;
+  cout << "Area = " << pi * r * r << endl;
+  cout << "Perimeter = " << 2 * pi * r << endl;
+
+
+  return 0;
+}
+
+//ZADACHA 7
+
+int main() {
+
+  double x1;
+  cin >> x1;
+  double y1;
+  cin >> y1;
+  double x2;
+  cin >> x2;
+  double y2;
+  cin >> y2;
+// namirane v koordinatnata sistema s funkciq FABS:
+  double width = fabs(x2 - x1);
+  double height = fabs(y2 - y1);
+  cout << "Area = " << width * height << endl;
+  cout << "Perimeter = " << 2 * (width + height) << endl;
+
+
+  return 0;
+}
