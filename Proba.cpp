8a7c67102fa7ca95ea5442cc74b9c7ad58ce5a24
SVP <140693336+SkySweepS@users.noreply.github.com>
2024-01-06 22:12:04 +0200

    Update Proba.cpp

--- Proba.cpp
+++ Proba.cpp
@@ -20,13 +20,63 @@ int main() {
   int wantedJumpHigh;
   cin >> wantedJumpHigh;
   double wantedJump = wantedJumpHigh - 30;
+  
   int jumpHigh;
+  cin >> jumpHigh;
+  
+  int count = 0;
+  int count2 = 0;
 
-  while(jumpHigh <= wantedJumpHigh){
-    cin >> jumpHigh;
+  if(jumpHigh > wantedJump ){
+    count++;
+    wantedJump += 5;
+    count2 = 0;
+    
   }
+  else if(jumpHigh <= wantedJumpHigh && wantedJump <= wantedJumpHigh){
+    count++;
+    count2++;
+
+  }
+  
+  
+  while(jumpHigh <= wantedJumpHigh && wantedJump < wantedJumpHigh){
+     
 
 
+    cin >> jumpHigh;
+    
+    if(wantedJump < jumpHigh){
+      count++;
+      wantedJump += 5;
+      count2 = 0;
+    }
+    else if(wantedJump>= jumpHigh){
+      count++;
+      count2++;
+       if(count2 == 3){
+        
+      break;
+    }
+      
+    }
+    if(count2 == 3){
+      break;
+    }
+   if(jumpHigh > wantedJumpHigh){
+    
+    break;
+   }
+     
+  }
+ 
+  if(jumpHigh > wantedJumpHigh){
+    cout << "Tihomir succeeded, he jumped over " << wantedJumpHigh << "cm after " << count << " jumps." << endl;
+  
+  }
+  if(count2 == 3){
+    cout << "Tihomir failed at " << wantedJump << "cm after " << count << " jumps." << endl;
+  }
 
   return 0;
 
