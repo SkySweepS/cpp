3777cffacbad8ce3230a12548db5d626a34ede5b
SVP <140693336+SkySweepS@users.noreply.github.com>
2024-01-06 19:49:21 +0200

    Update Proba.cpp

--- Proba.cpp
+++ Proba.cpp
@@ -1,71 +1,46 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <cmath>
-#include <cstdlib>
-#include <stdlib.h>
-#include <ctime>
-#include <climits>
-#include <cstring>
-
 
 using namespace std;
 
-
-
-
-
 int main() {
+    int wantedJumpHigh;
+    cin >> wantedJumpHigh;
 
-  int wantedJumpHigh;
-  cin >> wantedJumpHigh;
-  int jumpHigh;
-  cin >> jumpHigh;
-  double wantedJump = wantedJumpHigh - 30;
-  int count = 0;
-  int count2 = 0;
-
-  if(jumpHigh > wantedJump){
-    count++;
-    wantedJump += 5;
-    count2 = 0;
-  }
-  else if(wantedJump>=jumpHigh){
-    count++;
-    count2++;
-
-  }
-  
-  while(jumpHigh <= wantedJumpHigh){
-    
+    int jumpHigh;
     cin >> jumpHigh;
-    if(wantedJump < jumpHigh){
-      count++;
-      wantedJump += 5;
-      count2 = 0;
-    }
-    else if(wantedJump>= jumpHigh){
-      count++;
-      count2++;
-       if(count2 == 3){
-        cout << "Tihomir failed at " << wantedJump << "cm after " << count << " jumps." << endl;
-      break;
-    }
-      
-    }
-   
-    
-  }
-  if(jumpHigh > wantedJumpHigh){
-    cout << "Tihomir succeeded, he jumped over " << wantedJumpHigh << "cm after " << count << " jumps." << endl;
-  return 0;
-  }
+    double wantedJump = wantedJumpHigh - 30;
+    int count = 1;
+    int count2 = 0;
 
-  return 0;
+    if (jumpHigh >= wantedJump) {
+        wantedJump += 5;
+        count2++;
+    }
 
+    while (jumpHigh <= wantedJumpHigh) {
+        count++;
+
+        if (wantedJump > jumpHigh) {
+            count2++;
+            if (count2 == 3) {
+                cout << "Tihomir failed at " << wantedJump << "cm after " << count << " jumps." << endl;
+                return 0;
+            }
+        }
+
+        cin >> jumpHigh;
+
+        if (jumpHigh > wantedJumpHigh) {
+            cout << "Tihomir succeeded, he jumped over " << wantedJumpHigh << "cm after " << count << " jumps." << endl;
+            return 0;
+        }
+
+        if (wantedJump >= jumpHigh) {
+            count2 = 0;
+        }
+
+        wantedJump += 5;
+    }
+
+    return 0;
 }
-
-  
-    
-
-    
