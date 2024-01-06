50a3b284b569b474dea8b58595aa976ea6d460c1
SVP <140693336+SkySweepS@users.noreply.github.com>
2024-01-06 19:38:41 +0200

    Update Proba.cpp

--- Proba.cpp
+++ Proba.cpp
@@ -17,52 +17,48 @@ using namespace std;
 
 int main() {
 
-  string player1Name;
-  cin >> player1Name;
-  string player2Name;
-  cin >> player2Name;
+  int wantedJumpHigh;
+  cin >> wantedJumpHigh;
+  int jumpHigh;
+  cin >> jumpHigh;
+  double wantedJump = wantedJumpHigh - 30;
+  int count = 1;
+  int count2 = 0;
 
-  string endGame = " ";
-  string card = " ";
+  if(jumpHigh > jumpHigh){
+    count++;
+    wantedJump += 5;
+    count2 = 0;
+  }
+  else if(wantedJump>=jumpHigh){
+    count++;
+    count2++;
 
-  double player1Points = 0;
-  double player2Points = 0;
-
-  while(true){
+  }
+  
+  while(jumpHigh <= wantedJumpHigh){
     
-    getline(cin >> ws, endGame);
+    cin >> jumpHigh;
+    if(wantedJump < jumpHigh){
+      count++;
+      wantedJump += 5;
+      count2 = 0;
+    }
+    else if(wantedJump>= jumpHigh){
+      count++;
+      count2++;
+       if(count2 == 3){
+        cout << "Tihomir failed at " << wantedJump << "cm after " << count << " jumps." << endl;
+      break;
+    }
+      
+    }
+   
     
-    if(endGame == "End of game" || card == "End of game"){
-      cout << player1Name << " has " << player1Points << " points" << endl;
-      cout << player2Name << " has " << player2Points << " points" << endl;
-      break;
-
-    }
-    getline(cin >> ws, card);
-    if(endGame[0] > card[0]){
-      player1Points += endGame[0] - card[0];
-
-    }
-    else if(endGame[0] < card[0]){
-      player2Points += card[0] - endGame[0];
-    }
-    else if(endGame[0] == card[0]){
-      getline(cin, endGame);
-      getline(cin, card);
-      if(endGame[0] > card[0]){
-        cout << "Number wars!" << endl;
-        cout << player1Name << " is winner with " << player1Points << " points" << endl;
-        
-      }
-      else if(endGame[0] < card[0]){
-        cout << "Number wars!" << endl;
-        cout << player2Name << " is winner with " << player2Points << " points" << endl;
-        
-      }
-      break;
-      }
-
-    }
+  }
+  if(jumpHigh > wantedJumpHigh){
+    cout << "Tihomir succeeded, he jumped over " << wantedJumpHigh << "cm after " << count << " jumps." << endl;
+  }
 
   return 0;
 
