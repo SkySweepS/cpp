c192eda6fef90a5c4972cb12b80eb1a7e3da684f
SVP <140693336+SkySweepS@users.noreply.github.com>
2024-01-06 20:11:54 +0200

    Update Proba.cpp

--- Proba.cpp
+++ Proba.cpp
@@ -1,46 +1,38 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <stdlib.h>
+#include <ctime>
+#include <climits>
+#include <cstring>
+
 
 using namespace std;
 
+
+
+
+
 int main() {
-    int wantedJumpHigh;
-    cin >> wantedJumpHigh;
 
-    int jumpHigh;
+  int wantedJumpHigh;
+  cin >> wantedJumpHigh;
+  double wantedJump = wantedJumpHigh - 30;
+  int jumpHigh;
+
+  while(jumpHigh <= wantedJumpHigh){
     cin >> jumpHigh;
-    double wantedJump = wantedJumpHigh - 30;
-    int count = 1;
-    int count2 = 0;
+  }
 
-    if (jumpHigh >= wantedJump) {
-        wantedJump += 5;
-        count2++;
-    }
 
-    while (jumpHigh <= wantedJumpHigh) {
-        count++;
 
-        if (wantedJump > jumpHigh) {
-            count2++;
-            if (count2 == 3) {
-                cout << "Tihomir failed at " << wantedJump << "cm after " << count << " jumps." << endl;
-                return 0;
-            }
-        }
+  return 0;
 
-        cin >> jumpHigh;
-
-        if (jumpHigh > wantedJumpHigh) {
-            cout << "Tihomir succeeded, he jumped over " << wantedJumpHigh << "cm after " << count << " jumps." << endl;
-            return 0;
-        }
-
-        if (wantedJump >= jumpHigh) {
-            count2 = 0;
-        }
-
-        wantedJump += 5;
-    }
-
-    return 0;
 }
+
+  
+    
+
+    
