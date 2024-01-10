27c46858de6a68e2c557331ade9a8409aca85f12
SVP <140693336+SkySweepS@users.noreply.github.com>
2024-01-10 17:42:55 +0200

    Update Proba.cpp

--- Proba.cpp
+++ Proba.cpp
@@ -6,64 +6,49 @@
 #include <stdlib.h>
 #include <ctime>
 #include <climits>
-#include <sstream>
 
 
 
 using namespace std;
 
-//Basketball Equipment
-
-
 int main() {
+	
+ double vaucher;
+ cin >> vaucher;
+ cin.ignore();
+ string bought = " ";
+ double price = 0;
+ int ticket = 0;
+ int other = 0;
+ 
+ 
+ 
+ while(bought != "End" || vaucher < 0){
+ 	getline(cin, bought);
+	 int a = bought[0] + bought[1];
+    int b = bought[0];
 
-  string nameOfTournament = " ";
-  int games;
+	 
+	 if(bought.length() > 8){
+	 	price = a;
+		 ticket++;
+	 }
+	 else if(bought.length() <= 8){
+	 	price = b;
+		 other++;
+	 }
+	
+	
+	 vaucher -= price;
+	 
+	 cout << vaucher << endl;
+	 cout << a << endl;
+ cout << b << endl;	
+ }
 
-  int winCount = 0;
-  int loseCount = 0;
-  int gamesCount = 0;
-  
-  
 
-  while(nameOfTournament != "End of tournament"){
-    
-    getline(cin, nameOfTournament);
-    if(nameOfTournament == "End of tournament"){
-      break;
-    }
-    getline(cin, games);
-    for(int i = 1; i <= games; i++){
-      int points1;
-      getline(cin, points1);
-      int points2;
-      getline(cin, points2);
-      gamesCount++;
-      if(points1 > points2){
-        winCount++;
-        
-        cout << "Game " << gamesCount << " of tournament " << nameOfTournament << ": win with " << points1 - points2 << " points." << endl;
-        cout << winCount << "win" << endl;
-      }
-      else if(points2 > points1){
-        loseCount++;
-        cout << "Game " << gamesCount << " of tournament " << nameOfTournament << ": lost with " << points2 - points1 << " points." << endl;
-        cout << loseCount << "lose" << endl;
-              }
-    }
-  }
-
-  double winPercent = winCount / games * 100;
-  double losePercent = loseCount / games * 100;
-
-  cout << winPercent << "% matches win" << endl;
-  cout << losePercent << "% matches lost" << endl;
+cout << ticket << endl;
+cout << other << endl;
 
   return 0;
 }
-
-    
-
-    
-
-  
