eaa7fce986888eeb779283eb6ffc1d29f9a302aa
SVP <140693336+SkySweepS@users.noreply.github.com>
2024-01-27 22:46:15 +0200

    Update Proba.cpp

--- Proba.cpp
+++ Proba.cpp
@@ -6,48 +6,35 @@
 #include <stdlib.h>
 #include <ctime>
 #include <climits>
-#include <algorithm>
+#include <math.h>
 
-struct Movie {
-  string name;
-  double rating;
-};
+
+
 
 using namespace std;
 
+
 int main() {
-	
-	int numberOfMovies;
-  cin >> numberOfMovies;
 
-  double ratingTotal = 0;
-  vector<Movie> ratingVector;
+    int seats;
+    cin >> seats;
+    string movie = "";
+    int takenSeats;
+    while(1){
+        getline(cin, movie);
+        if(movie == "Movie time!"){
+            break;
+        }
+        takenSeats += stoi(movie);
+        cout << takenSeats;
+        if(seats < 0){
+            break;
+        }
+        
 
-  cin.ignore();
+    }
 
-  for(int i = 0; i < numberOfMovies; i++){
-    Movie movie;
-    getline(cin, movie.name);
-    cin >> movie.rating;
-    ratingVector.push_back(movie);
-    ratingTotal += movie.rating;
+     
 
-
-
-
-    cin.ignore();
-  }
-
-  auto minMovie = max_element(ratingVector.begin(), ratingVector.end(), [](const Movie& a, const Movie& b){
-    return a.rating < b.rating;
-  });
-    auto maxMovie = max_element(ratingVector.begin(), ratingVector.end(), [](const Movie& a, const Movie& b){
-      return a.rating < b.rating;
-    });
-
-    cout << maxMovie.name << " is with highest rating: " << maxMovie->rating << endl;
-    cout << minMovie.name << " is with lowest rating: " << minMovie->rating << endl;
-
-
-    return 0;
-  }
+  return 0;
+}
