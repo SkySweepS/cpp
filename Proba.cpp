c893b2e0eb4b1a5dfcbf3f3c5efb0170d6cde2e2
SVP <140693336+SkySweepS@users.noreply.github.com>
2024-01-30 23:30:35 +0200

    Update Proba.cpp

--- Proba.cpp
+++ Proba.cpp
@@ -1,40 +1,15 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <cmath>
-#include <cstdlib>
-#include <stdlib.h>
-#include <ctime>
-#include <climits>
-#include <math.h>
+n = int(input())
+pri = ""
+bol = False
+for i in range(0, n):
+    string = input()
+    for i1 in string:
+        if i1 == ',' or i1 == '.' or i1 == '_':
+            bol = True
+            pri = f"{string} is not pure!"
+    if bol:
+        print(pri)
+    else:
+        print(f"{string} is pure.")
 
 
-
-
-using namespace std;
-
-
-int main() {
-
-    int seats;
-    cin >> seats;
-    string movie = "";
-    int takenSeats;
-    while(1){
-        getline(cin, movie);
-        if(movie == "Movie time!"){
-            break;
-        }
-        takenSeats += stoi(movie);
-        cout << takenSeats;
-        if(seats < 0){
-            break;
-        }
-        
-
-    }
-
-     
-
-  return 0;
-}
